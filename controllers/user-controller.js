c7c0f6b8263b7db334371446ccf4c0f64d97c827
Ryely <ryely.moore@outlook.com>
2021-09-25 21:49:05 -0600

    set up basic controller skeletons for user and thought

---
+++ controllers/user-controller.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    // GET all users
+
+    // GET single user
+
+    // POST new user
+
+    // PUT to update user by _id
+
+    // DELETE to remove user by _id (remove users associated thoughts)
+
+    // POST to add a new friend to a user's friend list
+
+    // DELETE to remove a friend from a user's friend list
+}
+
+module.exports = userController;
